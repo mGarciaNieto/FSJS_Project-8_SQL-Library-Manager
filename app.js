e268f46c284400438ec60245861c24c3d9e66a79
Manuel Garcia-Nieto <mgarcianieto@gmail.com>
2023-08-16 21:22:08 +0200

    Add step 6

--- app.js
+++ app.js
@@ -41,23 +41,33 @@ app.use('/users', usersRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-	
+	//res.status(404).render('page-not-found')
+	const err = new Error()
+	err.status = 404
+	err.message = 'Page not found!'
+	next(err)
 })
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use(function (error, req, res, next) {
 	console.log('Global error handler called')
 	console.log(err.status)
 	// set locals, only providing error in development
-	err.status = err.status || 500
-	err.message = err.message || 'Sorry! Something went wrong. Please try again later!'
 
-	res.locals.message = err.message
-	res.locals.error = req.app.get('env') === 'development' ? err : {}
+	/* 	res.locals.message = err.message
+	res.locals.error = req.app.get('env') === 'development' ? err : {} */
 
 	// render the error page
-	res.status(err.status || 500)
-	res.render('error')
+	/* 	res.status(err.status || 500)
+	res.render('error') */
+
+	if (error.status === 404) {
+		res.status(404).render('page-not-found', { error, title: 'Page Not Found!' })
+		console.log('404 Error Occured!')
+	} else {
+		error.message = error.message || 'Uh no! Looks like something went wrong on the server. Return to Homepage.'
+		res.status(error.status || 500).render('error', { error, title: 'Server Error!' })
+	}
 })
 
 module.exports = app
